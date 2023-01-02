8402395a20952c7efd4488b6adf1cc959831c929
Alexandr Bochkaev <onebok@yandex.ru>
2023-01-02 14:41:05 +0700

    Комитим task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Задача 10.
+// Напишите программу, которая на вход
+// 1. принимает трехзначное число и
+// 2. на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918  -> 1
+// 
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <=1000)
+{
+   Console.WriteLine($"Вторая цифра введенного числа {(number /10)% 10} ");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
+
