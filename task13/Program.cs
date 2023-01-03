2d58e029aded324d78e88a4e90b823fd05301ff8
Alexandr Bochkaev <onebok@yandex.ru>
2023-01-03 20:32:18 +0700

    Комитим task13

---
+++ task13/Program.cs
@@ -0,0 +1,31 @@
+﻿//   Задача 13.
+// Напишите программу, которая на вход
+// 1. принимает число и 
+// 2. выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// 
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number >= 100 && number <=1000) || (number >= -1000 && number <=-100))
+
+    Console.WriteLine($"Третья цифра введенного числа {number % 10} "); //)
+
+if (number >0 && number < 100)
+    Console.WriteLine(" У введенного числа третьей цифры нет ");
+
+ //if (number > 1000)
+    
+while (number > 1000 || number < -1000)
+{
+    do
+    {
+         number = number /10;   
+    } while ((number > 1000 || number < -1000));
+    
+    Console.WriteLine($"Третья цифра введенного числа {number % 10} ");
+}
