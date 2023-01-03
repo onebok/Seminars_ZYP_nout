f10ae4e208467231581ba691ffa40059df880b3d
Alexandr Bochkaev <onebok@yandex.ru>
2023-01-03 19:19:07 +0700

    Комитим task08

---
+++ Task08/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 08
+// Напишите программу, которая 
+// 1. принимает на вход число (N)
+// а на выходе показывает все четные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6,8
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = number;
+counter = 2;
+while (counter <= number)
+{
+    Console.Write(counter + " ");
+    counter = counter + 2;
+}
