247d13d8fdc541ad434a2f4c4f4bf5bd1f82f908
Alexandr Bochkaev <onebok@yandex.ru>
2023-01-06 13:07:04 +0700

    Комитим task15

---
+++ task15/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Задача 15.
+// Напишите программу, которая принимает на вход
+// 1. цифру, обозначающую день недели,
+// 2. и проверяет, является ли этот день выходным.
+// 6 -> да 
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Введите целое число от 1 до 7: ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+
+if ((weekDay >= 1 & weekDay <= 5))
+{
+   Console.WriteLine("Рабочий день"); 
+} 
+   
+if (weekDay == 6 || weekDay == 7)
+{
+   Console.WriteLine("Выходной день");  
+}
+
+if (weekDay < 1)
+{
+   Console.WriteLine("Число введено неверно "); 
+}
+if (weekDay >7)
+{
+   Console.WriteLine("Число введено неверно"); 
+}
+// Почему-то не срабатывает запись
+// if (weekDay < 1 && weekDay >7)
+// {
+//    Console.WriteLine("Число введено неверно"); 
+// }
