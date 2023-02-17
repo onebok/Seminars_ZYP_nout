3ee47b5f084288b0e27e5eb0fc1f6522f143b63e
Alexandr Bochkaev <onebok@yandex.ru>
2023-02-17 21:53:27 +0700

    Комитим решение task35 количество двузначных чисел в массиве

---
+++ task35/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 35: 
+// Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+//Console.WriteLine("Введите число, которое надо найти в массиве ");
+//int number = Convert.ToInt32(Console.ReadLine());
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size]; //array.Length==size
+    var rnd = new Random();
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = rnd.Next(min, max+1);
+}
+    return array;    
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length -1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");   
+    }
+    Console.WriteLine("]");
+} 
+int QuantutyElemInRange(int[] array, int min, int max)
+{
+    int counter = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= min && array[i] <= max) counter++;
+    }
+    return counter;
+}
+
+int[] arr = CreateArrayRndInt(123, 0, 1000);
+PrintArray(arr);
+int quantutyElemInRange = QuantutyElemInRange(arr, 10,99);
+System.Console.WriteLine(quantutyElemInRange);
