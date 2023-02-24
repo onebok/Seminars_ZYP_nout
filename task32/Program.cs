43bacc07513c2fa2a48cd61300e165547ec91014
Alexandr Bochkaev <onebok@yandex.ru>
2023-02-24 21:38:47 +0700

    Решение task32 смена знаков элементов массива

---
+++ task32/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] newArr = RandomArray(12);
+PrintArray(newArr);
+PrintArray(RepLaceNum(newArr));
+
+int[] RepLaceNum(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(-9,10);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+    System.Console.WriteLine();
+}
+
