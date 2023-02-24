734639eca37c1c37bb4295e76a9ee37e863a3714
Alexandr Bochkaev <onebok@yandex.ru>
2023-02-24 21:22:27 +0700

    Решение task35 поиск элементов массива в заданном интервале

--- task35/Program.cs
+++ task35/Program.cs
@@ -1,11 +1,44 @@
-﻿int FindNumInInterval(int[] array)
+﻿//Задача 35.
+// Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, значения которых лежат 
+//в отрезке [10,99]. 
+
+int[] newArr = RandomArray(123);
+PrintArray(newArr);
+System.Console.WriteLine(FindNumInInterval(newArr, 10, 99));
+
+
+int FindNumInInterval(int[] array, int num1, int num2)
 {
     int count = 0;
     for(int i = 0; i < array.Length; i++)
     {
-        if(array[i] >= 10 && array[i] <= 99)
+        if(array[i] >= num1 && array[i] <= num2)
         {
-            count++;   
+            count++;
         }
     }
+    return count;
+}
+
+
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(1,1000);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+    System.Console.WriteLine();
 }
